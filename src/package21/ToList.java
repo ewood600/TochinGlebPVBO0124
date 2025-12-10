6f69c01a96fe82954b992a319f782292fa058888
ewood600 <tochin06@inbox.ru>
2025-12-10 11:23:53 +0300

    1

--- src/package21/ToList.java
+++ src/package21/ToList.java
@@ -1,7 +1,20 @@
 package package21;
 
-import java.nio.file.Files;
+import java.io.File;
+import java.util.ArrayList;
+import java.util.Arrays;
 
 public class ToList {
-
+    public static void main(String[] args) {
+        File dir = new File("C:\\Users\\tochi\\IdeaProjects\\untitled2\\src\\package21");
+        String[] catalogue = {"A", "B", "C", "D", "F"};
+        ArrayList<File> lst = new ArrayList<>();
+        for (String s1: catalogue){
+            File file = new File(dir, s1);
+            lst.add(file);
+        }
+        for (File s: lst){
+            System.out.println(s);
+        }
+    }
 }
